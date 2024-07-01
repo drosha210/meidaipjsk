068519f2524ea21137ec4c23370561b4b1742f35
drosha210 <158275453+drosha210@users.noreply.github.com>
2024-07-01 13:42:06 +0900

    Delete checked.js

--- checked.js
+++
@@ -1,4 +0,0 @@
-$(function(){
-  $("input[type='checkbox']").on('change', function () {if ($(".toggle-menu-botton:checked").length > 0) {
-    $(".menu").addClass("active");} 
-  else {$(".menu").removeClass("active");}});});
